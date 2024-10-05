2345643a33a2592aea6611a3d30979cf4509e193
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-10-06 02:40:58 +0530

    Find smallest letter greater than target

---
+++ DSA_Searching/CeilingUsingBs.java
@@ -0,0 +1,11 @@
+package COM.DSA_Searching;
+import  java.util.*;
+
+public class CeilingUsingBs {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,5};
+        int target = 5;
+
+
+    }
+}
