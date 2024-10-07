70ca90df8efbe2dc4e8e4e08dc6f9b437ebbe5ac
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-10-08 01:12:58 +0530

    Peak Index in a Mountain Array.

--- Vedant_LeetCode/MountainPeakElement_852.java
+++ Vedant_LeetCode/MountainPeakElement_852.java
@@ -4,23 +4,24 @@ package COM.Vedant_LeetCode;
 
 public class MountainPeakElement_852 {
     public static void main(String[] args) {
-        int [] arr = {1,2,5,8,7,4,2,0};
+        int[] arr = {1, 2, 5, 8, 7, 4, 2, 0};
+        System.out.println(PeakElement(arr));
 
-
-
-        int start = 0;
-        int end = arr.length-1;
-        while(start<end){
-            int mid = start+(end-start)/2;
-            if(arr[mid]>arr[mid+1]){
-                end = mid ;
-            }
-            else {
-                start = mid +1;
-            }
-
-
-        }
-        System.out.println(start);// ans will be end == start so we also print end for the answer
     }
+        public static int PeakElement( int[] arr){
+            int start = 0;
+            int end = arr.length - 1;
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] > arr[mid + 1]) {
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+
+
+            }
+            return end;// ans will be end == start so we also print end for the answer
+        }
+
 }
