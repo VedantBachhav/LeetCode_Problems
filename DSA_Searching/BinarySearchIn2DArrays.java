2cca184746f8aaceb7c02947b524ea80717baaf4
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-10-25 00:42:25 +0530

    Search in 2D Arrays/ matrix using bs.

---
+++ DSA_Searching/BinarySearchIn2DArrays.java
@@ -0,0 +1,11 @@
+package COM.DSA_Searching;
+//Search in 2D arrays and matrix using Binary Search
+public class BinarySearchIn2DArrays {
+    public static void main(String[] args) {
+        int  [][] arr={
+                {1,2,3,4},
+                {1,5,9,8},
+                {4,5,8,12,19}
+        };
+    }
+}
