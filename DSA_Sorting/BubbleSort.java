ebd3ec49148b617fcba19fdb0e3a075f1c14c52d
Vedant Devidas Bachhav <118623141+VedantBachhav@users.noreply.github.com>
2024-10-29 23:56:46 +0530

    Implementation of bubble sort.

--- DSA_Sorting/BubbleSort.java
+++ DSA_Sorting/BubbleSort.java
@@ -19,7 +19,7 @@ public class BubbleSort {
 
         for (int tern = 0; tern < arr.length - 1; tern++) {    // First loop for pass.
             for (int j = 0; j < arr.length - 1 - tern; j++) {  // second loop for iterating elements.
-                if (arr[j] > arr[j+1]) {                       // check if next element is small that current element then swap
+                if (arr[j] > arr[j+1]) {                       // check if next element is small than current element then swap.
                     int swap = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = swap;
