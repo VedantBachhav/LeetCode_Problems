4d2cf767532f99dec221e22e43c42a5e074e510b
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-12-06 23:53:18 +0530

    Find all duplicates in array using cycle sort in range 1 to N.

---
+++ Vedant_LeetCode/Problem442FindAllDuplicates.java
@@ -0,0 +1,19 @@
+package COM.Vedant_LeetCode;
+
+// Solved problem using cycle sort.
+
+//Problem link :
+
+//Solution link :
+
+import java.util.Arrays;
+
+public class Problem442FindAllDuplicates {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,3,4,5,5};
+        System.out.println(Arrays.toString(findAllDuplicates(arr)));
+    }
+    public static int[] findAllDuplicates(int [] nums){
+
+    }
+}
