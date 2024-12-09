b628f5ad4d2ac9d94aeea377f4bf45a4150f361c
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-12-09 23:47:02 +0530

    Arrenge coins using bs

---
+++ Vedant_LeetCode/Problem441ArrengingCoins.java
@@ -0,0 +1,12 @@
+package COM.Vedant_LeetCode;
+
+public class Problem441ArrengingCoins {
+    public static void main(String[] args) {
+        int n =8;
+        System.out.println(arrangeCoins(n));
+    }
+    public static int arrangeCoins(int n) {
+        int count = 0;
+
+    }
+}
