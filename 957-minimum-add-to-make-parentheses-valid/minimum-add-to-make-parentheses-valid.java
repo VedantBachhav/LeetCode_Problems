79a3627bcd95bdcc8a68e4bd775958501601701e
Vedant Devidas Bachhav <118623141+VedantBachhav@users.noreply.github.com>
2025-07-29 01:54:17 +0530

    Time: 2 ms (26.81%) | Memory: 41.6 MB (45.60%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,21 +1,37 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         Stack<Character> stack = new Stack<>();
-        int size = 0;
-        for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                stack.push(ch);
-                size++;
-            } else {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                    size--;
-                } else {
-                    size++;
-                }
-            }
-        }
+        // int size = 0;
+        // for (char ch : s.toCharArray()) {
+        //     if (ch == '(') {
+        //         stack.push(ch);
+        //         size++;
+        //     } else {
+        //         if (!stack.isEmpty()) {
+        //             stack.pop();
+        //             size--;
+        //         } else {
+        //             size++;
+        //         }
+        //     }
+        // }
 
-        return size;
-    }
-}
+        // return size;
+
+        for (char ch : s.toCharArray()) {
+            if(ch == ')'){
+              if (!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }
+                else{
+                    stack.push(ch);
+                }
+        
+            }
+                else{
+                    stack.push(ch);
+                }
+        }
+            
+    return stack.size();
+}}
