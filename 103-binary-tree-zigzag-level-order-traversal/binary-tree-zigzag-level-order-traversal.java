01b62a33bf50819fad1a62773a8f8343e5934cfc
Vedant Devidas Bachhav <118623141+VedantBachhav@users.noreply.github.com>
2025-08-03 16:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (90.58%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,28 +15,60 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        // *********** time complexity : O(N)+O(N) used reverse method and space complexity : O(N)
+        // this approach is readable and better but not in-place - beats 64% only; ************
+        // List<List<Integer>> result = new ArrayList<>();
+        // if(root == null ) return result;
+
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.offer(root);
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     List<Integer> level = new ArrayList<>(size);
+        //     for(int i = 0; i<size; i++){
+        //         TreeNode curr = q.poll();
+        //         level.add(curr.val);
+        //         if(curr.left != null) q.offer(curr.left);
+        //         if(curr.right != null ) q.offer(curr.right);
+        //     }
+        //     result.add(level);
+        // }
+        // int len = result.size();
+        // for(int i = 0; i<len ; i++){
+        //     if(i%2==1){
+        //         Collections.reverse(result.get(i));
+        //     }
+        // }
+        // return result;
+
+
+        // *********** time complexity : O(N) not used reverse method and space complexity : O(N)
+        // this approach is optimized and in-place - beats 94%+ only; ************
+        
+
         List<List<Integer>> result = new ArrayList<>();
         if(root == null ) return result;
 
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
+        boolean flag = false;
         while(!q.isEmpty()){
             int size = q.size();
             List<Integer> level = new ArrayList<>(size);
             for(int i = 0; i<size; i++){
                 TreeNode curr = q.poll();
-                level.add(curr.val);
+                if(flag){
+                     level.addFirst(curr.val);
+                }else{
+                    level.addLast(curr.val);
+                }
                 if(curr.left != null) q.offer(curr.left);
                 if(curr.right != null ) q.offer(curr.right);
             }
             result.add(level);
+            flag = !flag;
         }
-        int len = result.size();
-        for(int i = 0; i<len ; i++){
-            if(i%2==1){
-                Collections.reverse(result.get(i));
-            }
-        }
+        
         return result;
     }
 }
